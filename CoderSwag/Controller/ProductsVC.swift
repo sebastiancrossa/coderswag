eeb7d27144e7a41d3e929356c40eb49294ed6de7
Sebastian C <crossasebastian@gmail.com>
2017-07-29 12:05:09 -0600

    Created file for products view controller

---
+++ CoderSwag/Controller/ProductsVC.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsVC.swift
+//  CoderSwag
+//
+//  Created by Sebastian Crossa on 7/29/17.
+//  Copyright © 2017 CROSS-A. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
