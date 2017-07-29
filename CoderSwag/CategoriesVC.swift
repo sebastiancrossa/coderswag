a5f79d56f41a9063cfcc72fe31cd23511b653847
Sebastian C <crossasebastian@gmail.com>
2017-07-29 10:03:53 -0600

    Added app assets

---
+++ CoderSwag/CategoriesVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoderSwag
+//
+//  Created by Sebastian Crossa on 7/29/17.
+//  Copyright © 2017 CROSS-A. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
